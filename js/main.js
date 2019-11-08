d4deda997aaae349181e0cc7c2c4dc9d35a1952a
Yaroslav Mikhaylik <HaselLoyance@gmail.com>
2019-11-08 08:35:42 -0600

    i had to do it to em

--- js/main.js
+++ js/main.js
@@ -7,9 +7,18 @@ window.isLandscape = function() {
 // Once the page is loaded, do stuff. Fired only once
 $(document).ready(function() {
     window.DB.createFakeData();
+
+    $('.dropdown-field').click(() => {
+        $('.dropdown-pane').show();
+    });
+
+    $('.dropdown-pane .dropdown-item').click(() => {
+         $('.dropdown-pane').hide();       
+    });
 });
 
 // Do stuff on window resize
 $(window).resize(function() {
+
     console.log(window.isLandscape());
 });
