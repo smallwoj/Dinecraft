4c8f040f258dfcb3ebcd2ebd6027f961a279b20f
Joshua Smallwood <joshua.f.smallwood@outlook.com>
2019-11-18 10:07:01 -0600

    it was one file dude

--- js/ui/tableSelector.js
+++ js/ui/tableSelector.js
@@ -1,4 +1,21 @@
 class TableSelector
 {
-    
+    constructor(insideElem, onSelect, tableData)
+    {
+        this.onSelect = onSelect;
+        this.data = tableData;
+
+        var el = $(`
+            <div class="table ui-style-1"></div>
+        `);
+        el.click(cbc(this, 0, this.onClick()));
+        this.ref = el.appendTo($(insideElem));
+        this.tableRef = this.ref.find(".table");
+
+    }
+
+    onClick()
+    {
+        this.onSelect(data);
+    }
 }
