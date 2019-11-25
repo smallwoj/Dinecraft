fe6c69df0424776f4cc1e6a2d3aeb42d11f66d75
Joshua Smallwood <joshua.f.smallwood@outlook.com>
2019-11-25 06:24:23 -0600

    CLEAN TABLES MR. KRABS

--- js/utils.js
+++ js/utils.js
@@ -15,7 +15,7 @@ window.makePopup = function(text, onAgree) {
 	$('body').prepend(`<div class="popup-overlay"></div>`);
 	$('body').prepend(`
 		<div class="popup ui-style-1">
-			<div class="popup-top"><h4>${text}</h4></div>
+			<div class="popup-top" align="center"><h4>${text}</h4></div>
 			<div class="popup-bottom">
 				<div class="popup-no ui-style-1"><h4>No</h4></div>
 				<div class="popup-yes ui-style-1"><h4>Yes</h4></div>
