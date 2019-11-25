91cd2fb30f359d7b802dd6039d6fcf3b08def217
Joshua Smallwood <joshua.f.smallwood@outlook.com>
2019-11-24 18:05:46 -0600

    Prettified the bills, started work on the Pay button

--- js/ui/bill.js
+++ js/ui/bill.js
@@ -3,14 +3,17 @@ class Bill
     constructor(insideElem, guestOrder)
     {
         this.guestOrder = guestOrder;
-        var el = $('<div class="bill ui-style-1"></div>');
+        var el = $('<div class="bill ui-style-1"><div class="bill-top"></div><div class="bill-bottom"></div></div>');
+        var top = el.find('.bill-top');
+        var bottom = el.find('.bill-bottom');
         this.totalPrice = 0;
         //create image
-        var img = $('<div class="guest-icon"></div>');
-        $(img).css('background', 'url(' + guestOrder.icon.source + '.png) no-repeat center center');
-        $(img).css('background-size', '30%');
-        console.log(img)
-        img.appendTo(el);
+        var guestIcon = $('<div class="guest-icon" align="center"><img src="'+this.guestOrder.icon.source + '.png"></div>');
+        $(guestIcon).css('width', '100%');
+        $(guestIcon).css('height', 'auto');
+        $(guestIcon.find('img')).css('width', '30%');
+        $(guestIcon.find('img')).css('max-width', '64px');
+        guestIcon.appendTo($(el));
 
         var items = $('<div class="items"></div>');
         //for each item in the order, add it to the list
@@ -20,22 +23,49 @@ class Bill
             var itemNameString = this.guestOrder.items[i].item.name;
             if(this.guestOrder.items[i].quantity > 1)
                 itemNameString+=' x'+this.guestOrder.items[i].quantity;
-            itemName.html('<h4>'+itemNameString+'</h4>');
+            itemName.html(itemNameString);
             itemName.appendTo(items);
-            //TODO: split prices and item name into separate divs
             var priceString ='$'+(this.guestOrder.items[i].item.price*this.guestOrder.items[i].quantity);
             var price = $('<div class="price" align="right"></div>');
-            //price.html('<font color="#218306"><h4>'+priceString+'</h4></font>');
-            price.html('<h4>'+priceString+'</h4>');
-            $(price).css('color','#123123');
+            price.html(priceString);
             price.appendTo(items);
             this.totalPrice += this.guestOrder.items[i].item.price*this.guestOrder.items[i].quantity;
         }
-        items.appendTo($(el));
+        items.appendTo($(top));
 
         var total = $('<div class="total" align="center"></div>')
-        total.html('<h4>TOTAL: $'+this.totalPrice)
-        total.appendTo(el);
+        total.html('<h4>TOTAL: <font color="#218306">$'+this.totalPrice+'</font>');
+        total.appendTo(bottom);
+
+        var paymentButton = $(`<div class="payment-button ui-style-1" align="center"><h4>Pay</h4></div>`);
+        paymentButton.appendTo(bottom);
+        $(bottom.find('.payment-button')).click(this.askPayment.bind(this));
+
+        top.appendTo(el);
+        bottom.appendTo(el);
         el.appendTo($(insideElem));
+    }
+
+    askPayment()
+    {
+        console.log(this);
+        $('body').prepend(`<div class="popup-overlay"></div>`);
+        $('body').prepend(`
+            <div class="popup ui-style-1">
+                <div class="popup-top"><h4>How will customer <img src="${this.guestOrder.icon.source}.png"> be paying?</h4></div>
+                <div class="popup-bottom">
+                    <div class="popup-cash ui-style-1"><h4>No</h4></div>
+                    <div class="popup-credit ui-style-1"><h4>Yes</h4></div>
+                    <div class="popup-debit ui-style-1"><h4>seirullkjfd</h4></div>
+                    <div class="popup-cancel ui-style-1"><h4>Cancel</h4></div>
+                </div>
+            </div>
+        `);
+        $($("body").find('.popup').find('img')).css('width', '24px');
+        $($("body").find('.popup').find('img')).css('image-rendering', 'pixelated');
+    
+        $('.popup-overlay').click(function(e) { $('.popup-overlay').remove(); $('.popup').remove(); });
+        $('.popup-no').click(function(e) { $('.popup-overlay').remove(); $('.popup').remove(); });
+        $('.popup-yes').click(function(e) { onAgree(); $('.popup-overlay').remove(); $('.popup').remove();});
     }
 } 
