3cafdbeb58057c769c63c9641601b3450e41ac7e
Joshua Smallwood <joshua.f.smallwood@outlook.com>
2019-11-25 23:14:24 -0600

    the final commit

--- js/ui/foodCard.js
+++ js/ui/foodCard.js
@@ -10,12 +10,12 @@ class FoodCard {
             <div class="food-card-top">
                 <img src=${menuItem.icon.source} style="width:50%; max-width:64px;"></img>
                 <div class="text"><h5 style="color:#5F5F5F">${menuItem.name}</h5></div>
-                <div class="price"><h5 style="color:#218306">$${menuItem.price}</h5></div>
+                <div class="price"><h5 style="color:#218306;">$${menuItem.price}</h5></div>
             </div>
             <div class="food-card-bottom">
-                <div class="quantity-counter" style="width: 100%; margin-bottom:5%;"></div>
-                <div class="additional-details"></div>
-                <div class="img-wrapper" style="position: relative; margin-left:auto; margin-right:auto; width:100%; margin-bottom:1%;"><img src="./img/expandthis.png" width=100%></div>
+            <div class="quantity-counter" style="width: 100%; margin-bottom:5%;"></div>
+            <div class="additional-details"></div>
+            <div class="img-wrapper" style="position: relative; margin-left:auto; margin-right:auto; width:100%; margin-bottom:5%;"><img src="./img/expandthis.png" width=100%></div>
             </div>
             </div>
         `);
@@ -51,6 +51,7 @@ class FoodCard {
         $(this.ref.find('.calories')).html(calories);
         $(this.ref.find('.img-wrapper').find('img')).css('transform', 'scale(1, -1)');
         $(this.ref.find('.img-wrapper')).click(this.showntDetailedStuff.bind(this));
+        //$(this.ref.find('.additional-details').find('h5')).css('font-size', '1.3rem');
     }
 
     showntDetailedStuff()
